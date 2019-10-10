0fdafe02d3b37f38de2a7295c63e602fe990d3cb
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2019-10-10 18:00:44 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -57,12 +57,14 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # -- Custom
-f = file('macros.tex')
+f = open('macros.tex','r')
 
 try:
 	pngmath_latex_preamble
 except NameError:
 	pngmath_latex_preamble = ""
 	
-for macro in f:
+for macro in f.readlines():
 	pngmath_lates_preamble += macro + '\n'
+	
+close(f)
