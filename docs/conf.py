d244b577390a2aa8828ddddbacee29a14491cb67
Juancho <juanjqogm@gmail.com>
2024-03-18 06:36:29 +0900

    [conf.py] Defined the bib file. This is required to run the make html command.

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Define the bib file location
+bibtex_bibfiles = ['references.bib']
 
 # -- Options for HTML output -------------------------------------------------
 
